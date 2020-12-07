d2b695a9dc5df030342bc892099eed4bd311a72e
Juan Carlos <contacto@audiomax.cl>
2020-12-06 22:22:43 -0300

    product add 2

---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import {Product} from '../../models/product'
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+
+  products : Product[] = [{
+    id: 1,
+    imagen: 'assets/img/product/1.jpg',
+    title: 'Detergente OMO',
+    categoria : 'Detergente Ropa',
+    descripcion: 'Detergente de 1kg ropa blanca y color',
+    price: 3000,
+  },
+  {
+    id: 2,
+    imagen: 'assets/img//product/2.jpg',
+    title: 'Detergente OMO',
+    categoria : 'Detergente Ropa',
+    descripcion: 'Detergente líquido 4.43 L',
+    price: 3000,
+  },
+  {
+    id: 3,
+    imagen: 'assets/img//product/3.jpg',
+    title: 'Detergente OMO',
+    categoria : 'Detergente Ropa',
+    descripcion: 'Detergente líquido 4.43 L',
+    price: 3000,
+  },
+  {
+    id: 4,
+    imagen: 'assets/img//product/4.jpg',
+    title: 'Detergente OMO',
+    categoria : 'Detergente Ropa',
+    descripcion: 'Detergente líquido 4.43 L',
+    price: 3000,
+  },
+  {
+    id: 5,
+    imagen: 'assets/img//product/5.jpg',
+    title: 'Detergente OMO',
+    categoria : 'Detergente Ropa',
+    descripcion: 'Detergente líquido 4.43 L',
+    price: 3000,
+  },
+  {
+    id: 6,
+    imagen: 'assets/img//product/6.jpg',
+    title: 'Detergente OMO',
+    categoria : 'Detergente Ropa',
+    descripcion: 'Detergente líquido 4.43 L',
+    price: 3000,
+  },
+]
+clickProduct(id: number){
+  console.log("id es:" + id)
+
+}
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
