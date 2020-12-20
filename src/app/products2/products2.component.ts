b8822ec0f57c4b8d9507e0eed1b870a584e24a77
Juan Carlos <contacto@audiomax.cl>
2020-12-20 16:57:46 -0300

    ROUTER ADD

---
+++ src/app/products2/products2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products2',
+  templateUrl: './products2.component.html',
+  styleUrls: ['./products2.component.css']
+})
+export class Products2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
