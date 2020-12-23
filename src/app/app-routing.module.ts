a490c36da0e8e19a830afe42e0c854f2e1155a3c
Juan Carlos <contacto@audiomax.cl>
2020-12-22 22:26:11 -0300

    prueba jveliz

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import {QuienesSomosComponent} from './quienes-somos/quienes-somos.component';
-import {Products2Component} from './products2/products2.component'
+import {Products2Component} from './products2/products2.component';
+import {IndicadoresComponent} from './indicadores/indicadores.component';
+
 import {ContactComponent} from './contact/contact.component';
-<<<<<<< HEAD
+
 import {LoginComponent} from './components/auth/login/login.component';
 import {RegisterComponent} from './components/auth/register/register.component';
 import {VerifyemailComponent} from './components/auth/verifyemail/verifyemail.component';
 
 import {AuthGuard} from "./auth.guard";
 
-=======
-import {IndicadoresComponent} from './indicadores/indicadores.component'
->>>>>>> aff0316829c8b8181f6657272e41401a9293eeb4
 
  //const routes: Routes = [];
 
@@ -23,11 +22,8 @@ import {IndicadoresComponent} from './indicadores/indicadores.component'
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard]  },
   { path: 'quienes', component: QuienesSomosComponent},
   { path: 'productos', component: Products2Component},
-<<<<<<< HEAD
-=======
   { path: 'indicadores', component: IndicadoresComponent},
 
->>>>>>> aff0316829c8b8181f6657272e41401a9293eeb4
   { path: 'contacto', component: ContactComponent},
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
